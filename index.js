6a0c7480de96804f75f0f4654669f2e41466b1cc
God'swill Sekav Apya <godswillapya@gmail.com>
2025-02-25 16:51:08 +0100

    update prompt in index.js to provide context for generative model

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function fileToGenerativePart(path, mimeType) {
 
 async function start() {
   const model = mathAi.getGenerativeModel({ model: "gemini-1.5-pro" });
-  const prompt = ""; //prompt is empty because the model is capable of reading prompt from image
+  const prompt = "Explain the answer"; //prompt is empty because the model is capable of reading prompt from image
   const imageParts = [fileToGenerativePart("math.jpg", "image/jpeg")];
   const result = await model.generateContent([prompt, ...imageParts]);
   const response = await result.response;
